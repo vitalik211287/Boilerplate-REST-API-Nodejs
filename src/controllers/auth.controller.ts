45a67d479987a54823e9ff856fa21c2b681bfc85
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2026-08-11 22:27:08 +0300

    Update auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,20 +4,28 @@ import prisma from "../../prisma/client.ts";
 import type { RegisterBody } from "../validators/auth.validators.ts";
 import bcrypt from "bcrypt";
 
-const hashPassword = async (password: string): Promise<string> => {
-  const saltRounds = 10;
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
-  return hashedPassword;
-}
-
 const registerUser = async (
   req: Request<{}, {}, RegisterBody>,
   res: Response,
 ) => {
-  const newUser = await prisma.user.create({ data: req.body });
-  res
-    .status(201)
-    .json({ id: newUser.id, email: newUser.email, name: newUser.name });
+  const { username, email, password, name } = req.body;
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: passwordHash,
+      name,
+    },
+  });
+
+  res.status(201).json({
+    id: newUser.id,
+    email: newUser.email,
+    name: newUser.name,
+  });
 };
 
 export { registerUser };
